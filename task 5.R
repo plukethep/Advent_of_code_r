f2401161cf741c4bbf590f40b1ce0740e7db26ff
plukethep <peterejkemp@gmail.com>
2021-12-05 22:08:08 +0000

    Add files via upload

---
+++ task 5.R
@@ -0,0 +1,15 @@
+# In this diagram, the top left corner is 0,0 and the bottom right corner is 9,9
+
+vents <- read.delim("test5.txt", sep=">", col.names=c("p1","p2"), header=FALSE) %>%
+  mutate(p1 = gsub(" -", "", p1),
+         p2 = gsub(" ", "", p2)) %>%
+  separate(p1, c("x1", "y1"), ",") %>%
+  separate(p2, c("x2", "y2"), ",") %>%
+  mutate(across(everything(),as.numeric)) #%>%
+  # filter(x1 == x2 | y1 == y2) # snip out to restore diagonals
+
+# get all intermediate locations  
+points <- unlist(map(data.frame(vents %>% t()), 
+                     function(x) paste0(seq(x[1], x[3]), ",", seq(x[2], x[4]))))
+
+data.frame(points) %>% group_by(points) %>% summarise(n = n()) %>% filter(n > 1) %>% nrow()
