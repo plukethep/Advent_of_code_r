7056ea1cbb6ce6d922917dee46ff5252ede54e06
plukethep <peterejkemp@gmail.com>
2021-12-05 22:09:11 +0000

    Update task 5.R

--- task 5.R
+++ task 5.R
@@ -1,12 +1,10 @@
-# In this diagram, the top left corner is 0,0 and the bottom right corner is 9,9
-
 vents <- read.delim("test5.txt", sep=">", col.names=c("p1","p2"), header=FALSE) %>%
   mutate(p1 = gsub(" -", "", p1),
          p2 = gsub(" ", "", p2)) %>%
   separate(p1, c("x1", "y1"), ",") %>%
   separate(p2, c("x2", "y2"), ",") %>%
   mutate(across(everything(),as.numeric)) #%>%
-  # filter(x1 == x2 | y1 == y2) # snip out to restore diagonals
+  # filter(x1 == x2 | y1 == y2) # snip out to restore diagonals for Part 1
 
 # get all intermediate locations  
 points <- unlist(map(data.frame(vents %>% t()), 
