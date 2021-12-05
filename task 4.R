193ff7ab98cd8ac34a1b4c68cbd4c3abc5fd3575
plukethep <peterejkemp@gmail.com>
2021-12-05 19:24:46 +0000

    Add files via upload

---
+++ task 4.R
@@ -0,0 +1,9 @@
+readLines("input4.txt")
+
+calls <- as.list(read.table("input4.txt", sep=",", nrows=1))
+
+tab1 <- read.table("input4.txt", nrows=5, skip=2)
+tab2 <- read.table("input4.txt", nrows=5, skip=8)
+tab3 <- read.table("input4.txt", nrows=5, skip=14)
+
+
