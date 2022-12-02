cb59e7309396e2003fb3845ef73f833eb37e6b87
plukethep <peterejkemp@gmail.com>
2022-12-02 16:10:17 +0000

    Update day2.R

--- 2022/day2.R
+++ 2022/day2.R
@@ -1,35 +1,26 @@
-### check for applicable matches
-
 library(tidyverse)
 
-input <- read_csv("C:/tmp/input2.txt", col_names = c("moves"))
-
-input <- input %>% 
-  mutate(p2 = gsub(".*[ ]", "", moves),
-         p1 = gsub("[ ].*", "", moves))
+input <- read_csv("C:/tmp/input2.txt", col_names = c("moves"))%>% 
+              mutate(p2 = gsub(".*[ ]", "", moves),
+                     p1 = gsub("[ ].*", "", moves))
 
 # vector shifter
 shifter <- function(x, n = 1){
   if (n == 0) x else c(tail(x, n), head(x, -n))
 }
 
-# number of symbols
+# create score pattern for all combos
 n <- 3
 
-# get all combinations of scores
+# get all combinations of letters
 combinations <- expand.grid(p1=c(LETTERS[1:n]),
                             p2=LETTERS[(27-n):26]) %>%
   mutate(moves = paste(p1, p2)) 
-
-# create score pattern
+# create scores
 combinations$winlose <- 
-  map(1:n-1,
-      ~{shifter(c(3,0,0), .x)}) %>% unlist() + 
-  map(1:n-1,
-      ~{shifter(c(0,0,6), .x)}) %>% unlist()
-combinations$move_score <- 
-  map(1:n,
-      ~{rep(.x,n)}) %>% unlist()
+  map(1:n-1, ~{shifter(c(3,0,0), .x)}) %>% unlist() + 
+  map(1:n-1, ~{shifter(c(0,0,6), .x)}) %>% unlist()
+combinations$move_score <- map(1:n, ~{rep(.x,n)}) %>% unlist()
 
 left_join(input, combinations) %>% 
   mutate(score = move_score + winlose) %>% 
@@ -37,9 +28,7 @@ left_join(input, combinations) %>%
 
 # Part 2
 # create result mapping
-result <- read_csv("X,0
-Y,3
-Z,6", col_names = c("result","outcome"))
+result <- data.frame(result=c("X","Y","Z"), outcome=c(0,3,6))
 
 # join input to the desired result mapping
 input <- left_join(input, result, by=c("p2"="result"))
