8a841805325e5230448b3bb50254dd919d1d4813
plukethep <peterejkemp@gmail.com>
2022-12-02 13:31:49 +0000

    Update day2.R

--- 2022/day2.R
+++ 2022/day2.R
@@ -1,3 +1,5 @@
+### check for applicable matches
+
 library(tidyverse)
 
 input <- read_csv("C:/tmp/input2.txt", col_names = c("moves"))
@@ -16,35 +18,33 @@ n <- 3
 
 # get all combinations of scores
 combinations <- expand.grid(p1=c(LETTERS[1:n]),
-            p2=LETTERS[(27-n):26]) %>%
-  summarise(moves = paste(p1, p2))
+                            p2=LETTERS[(27-n):26]) %>%
+  mutate(moves = paste(p1, p2)) 
 
 # create score pattern
-winlose <- 
+combinations$winlose <- 
   map(1:n-1,
-    ~{shifter(c(3,0,0), .x)}) %>% unlist() + 
+      ~{shifter(c(3,0,0), .x)}) %>% unlist() + 
   map(1:n-1,
-    ~{shifter(c(0,0,6), .x)}) %>% unlist()
-move_score <- 
+      ~{shifter(c(0,0,6), .x)}) %>% unlist()
+combinations$move_score <- 
   map(1:n,
       ~{rep(.x,n)}) %>% unlist()
 
-combinations$winlose <- winlose
-combinations$move_score <- move_score
-combinations$score <- combinations$move_score + combinations$winlose
-combinations <- combinations %>% 
-  mutate(p2 = gsub(".*[ ]", "", moves),
-         p1 = gsub("[ ].*", "", moves))
-left_join(input, combinations)$score %>% sum()
+left_join(input, combinations) %>% 
+  mutate(score = move_score + winlose) %>% 
+  pull(score) %>% sum()
 
 # Part 2
+# create result mapping
 result <- read_csv("X,0
 Y,3
 Z,6", col_names = c("result","outcome"))
 
+# join input to the desired result mapping
 input <- left_join(input, result, by=c("p2"="result"))
 
-# get moves and cacluate score
+# get moves and calculate score
 left_join(input, combinations, by=c("p1"="p1",
                                     "outcome"="winlose")) %>%
   mutate(total_score = outcome+move_score) %>%
