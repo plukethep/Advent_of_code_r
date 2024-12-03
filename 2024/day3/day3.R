e41c3f1228598cdb742d3558e5e5ec559f69f779
Peter Kemp <118382375+peterejkemp@users.noreply.github.com>
2024-12-03 14:09:25 +0000

    Update day3.R

--- 2024/day3/day3.R
+++ 2024/day3/day3.R
@@ -1,4 +1,4 @@
-text_file %>% str_remove_all("don[']t[(].*do[(]") %>% 
+muls <- text_file %>% str_remove_all("don[']t[(].*do[(]") %>% 
   str_extract_all("mul[(][0-9]+,[0-9]+[)]") %>% 
   unlist()
 x <- str_extract_all(muls, "[(][0-9]+") %>% str_remove_all("[(]") %>% as.numeric()
