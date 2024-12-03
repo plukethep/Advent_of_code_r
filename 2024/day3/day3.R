d28b64144f685852bf8462581a8bb495aa6aa5eb
Peter Kemp <118382375+peterejkemp@users.noreply.github.com>
2024-12-03 14:31:11 +0000

    Update day3.R

--- 2024/day3/day3.R
+++ 2024/day3/day3.R
@@ -1,4 +1,17 @@
-muls <- text_file %>% str_remove_all("don[']t[(].*do[(]") %>% 
+library(tidyverse)
+library(glue)
+library(here)
+text_file <- read_file(glue("{loc}data.txt"))
+
+# PART 1
+muls <- text_file %>% str_extract_all("mul[(][0-9]+,[0-9]+[)]") %>% unlist()
+x <- str_extract_all(muls, "[(][0-9]+") %>% str_remove_all("[(]") %>% as.numeric()
+y <- str_extract_all(muls, "[0-9]+[)]") %>% str_remove_all("[)]") %>% as.numeric()
+(x * y) %>% reduce(`+`)
+
+# PART 2
+muls <- text_file %>% str_remove_all("\\r\\n") %>%
+  str_remove_all("don[']t[(].*do[(]") %>% 
   str_extract_all("mul[(][0-9]+,[0-9]+[)]") %>% 
   unlist()
 x <- str_extract_all(muls, "[(][0-9]+") %>% str_remove_all("[(]") %>% as.numeric()
